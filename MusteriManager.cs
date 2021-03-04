674041f8506a38e9eac98002b1c72926bf2aaffa
BERNA NUR TAŞDEMİR <17050211051@ybu.edu.tr>
2021-03-04 19:58:53 +0300

    Add project files.

---
+++ MusteriManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Eklendi: " + musteri.Adi + musteri.Soyadi);
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("{0} Id'li {1} sistemden silindi.", musteri.Id, musteri.Adi + musteri.Soyadi);
+        }
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine("Müşterinin Adı:" + musteri.Adi);
+            Console.WriteLine("Müşterinin Soyadı:" + musteri.Soyadi);
+            Console.WriteLine("Müşteri Id:" + musteri.Id);
+            Console.WriteLine("Müşterinin Yaşı:" + musteri.Yasi);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Adi = "Berna";
+            musteri1.Soyadi = "Taşdemir";
+            musteri1.Id = 1145;
+            musteri1.Yasi = 32;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Adi = "Beyza";
+            musteri2.Soyadi = "Somuncu";
+            musteri2.Id = 1149;
+            musteri2.Yasi = 19;
+
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Adi = "Elif Azra";
+            musteri3.Soyadi = " Çanga";
+            musteri3.Id = 1146;
+            musteri3.Yasi = 4;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri3);
+            musteriManager.Sil(musteri1);
+            musteriManager.Listele(musteri2);
+        }
+    }
+}
